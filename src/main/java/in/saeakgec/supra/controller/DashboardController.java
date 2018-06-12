4219ca701d6c338005cc5cdaac5d956bf930d0db
deepanshut041 <deepanshut041@gmail.com>
2018-06-12 20:52:04 +0530

    Add onMouseClicked on pendingList

--- src/main/java/in/saeakgec/supra/controller/DashboardController.java
+++ src/main/java/in/saeakgec/supra/controller/DashboardController.java
@@ -6,10 +6,12 @@ import in.saeakgec.supra.service.DashboardService;
 import in.saeakgec.supra.util.HeaderRequestInterceptor;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 
@@ -24,19 +26,20 @@ public class DashboardController implements Initializable {
 
     @FXML
     private ListView pendingListView;
-//    @FXML
-//    private ListView completedListView;
+    @FXML
+    private ListView completedListView;
 
     ObservableList<Race> pendingObservableList = FXCollections.observableArrayList();
+    ObservableList<Race> completedObservableList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         dashboardService = new DashboardService();
         List<Race> races = dashboardService.getAdminRace();
-        for(Race race:races){
-            System.out.println(race.getName());
-        }
-        setPendingListView(races);
+        List<Race> pendingRaces = getPendingView(races);
+        List<Race> completedRaces = getCompletedView(races);
+        setPendingListView(pendingRaces);
+        setCompletedListView(races);
     }
 
     public void setPendingListView(List<Race> races){
@@ -54,7 +57,6 @@ public class DashboardController implements Initializable {
                         super.updateItem(item, empty);
 
                         if (item != null){
-                            System.out.println(item.getId());
                             DashboardData data = new DashboardData();
                             data.setInfo(item.getName(), item.getTime().toString());
                             data.setIndicaton( "btn-warning", "btn-xs");
@@ -65,12 +67,82 @@ public class DashboardController implements Initializable {
                         }
                     }
 
+
+
                 };
 
                 return cell;
             }
         });
 
+        pendingListView.setOnMouseClicked(new EventHandler<MouseEvent>(){
 
+            @Override
+            public void handle(MouseEvent arg0) {
+
+                ObservableList<Race> clickedRaces = pendingListView.getSelectionModel().getSelectedItems();
+                Race race = clickedRaces.get(0);
+                System.out.println(race.getName());
+            }
+
+        });
+
+
+    }
+
+    public void setCompletedListView(List<Race> races){
+        completedObservableList.setAll(races);
+        completedListView.setItems(completedObservableList);
+        completedListView.setCellFactory(new Callback<ListView<Race>, ListCell<Race>>(){
+
+            @Override
+            public ListCell<Race> call(ListView<Race> p) {
+
+                ListCell<Race> ncell = new ListCell<Race>(){
+
+                    @Override
+                    protected void updateItem(Race item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (item != null){
+                            DashboardData data = new DashboardData();
+                            data.setInfo(item.getName(), item.getTime().toString());
+                            data.setIndicaton( "btn-info", "btn-xs");
+                            setGraphic(data.getBox());
+                        }else {
+                            textProperty().unbind();
+                            textProperty().set("");
+                        }
+                    }
+
+                };
+
+                return ncell;
+            }
+        });
+
+
+    }
+    
+    public List<Race> getPendingView(List<Race> races){
+        ArrayList<Race> pendingRaces = new ArrayList<>();
+        for (Race race : races){
+            if((race.getStatus()).equals("pending")){
+                pendingRaces.add(race);
+            }
+        }
+        
+        return pendingRaces;
+    }
+
+    public List<Race> getCompletedView(List<Race> races){
+        ArrayList<Race> completedRaces = new ArrayList<>();
+        for (Race race : races){
+            if((race.getStatus()).equals("completed")){
+                completedRaces.add(race);
+            }
+        }
+
+        return completedRaces;
     }
 }
